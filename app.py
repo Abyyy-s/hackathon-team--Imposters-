6fc427fdd4c7bcf1e4193903686b6e2c61a1cb20
Abyyy-s <abisbiju321@gmail.com>
2026-03-02 23:21:13 +0530

    about to depoly

--- app.py
+++ app.py
@@ -468,4 +468,9 @@ if __name__ == '__main__':   # ✅ CORRECT
     print("\n🩸 LifeLink AI — Blood Emergency Response System")
     print(f"{'✅ Gemini AI active!' if GEMINI_API_KEY else '⚠️  No GEMINI_API_KEY — using fallback logic'}")
     print("📌 Open http://localhost:5000\n")
-    app.run(debug=True, port=5000)
+
+
+app.run(
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 5000))
+)
